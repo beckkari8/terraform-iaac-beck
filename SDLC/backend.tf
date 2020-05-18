eb70da4adc746eed7a35086f0cd37ca7a0c7b289
beckkari8 <karimovbektur@gmail.com>
2020-05-18 06:49:32 +0000

    0.

--- SDLC/backend.tf
+++ SDLC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "sdlc-beck-stage"
-key = "vet/us-west-2/sharedtools/Stage/stage.tfstate"
+key = "vet/us-west-2/sharedtools/Stage-beck/beck.tfstate"
 region = "us-west-2"
   }
 }
